6dc953b323a8d7f1c3e8aecacaa3db2a7520ba67
yambikov <yambikov@yandex.ru>
2024-01-28 22:49:06 +0300

    03 Header in progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 // import React, { useState } from "react"
-import { CurrentUserContext } from "../src/context/CurrentUserContext"
+import { CurrentUserContext } from "../../context/CurrentUserContext"
 import { Routes } from 'react-router-dom';
-import Main from "./components/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Main from "../Main";
+import Header from "../Header/Header";
+import Footer from "../Footer";
 import { Route } from "react-router";
 
 
